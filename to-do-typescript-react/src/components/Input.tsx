f9f7786ea029e31aeba87ffd3f652c8eb347ec35
Taufiq <taufiq9.2004@gmail.com>
2024-04-28 16:04:36 +0530

    setup

---
+++ to-do-typescript-react/src/components/Input.tsx
@@ -0,0 +1,15 @@
+
+
+const Input = () => {
+   const handleAdd = ( )=>{
+
+   }
+  return (
+    <div>
+        <input className="border-black border-[2px] p-1 m-1 w-1/3"/>
+        <button onClick={handleAdd} className="bg-black text-white p-1 border-black border-[2px]">Add</button>
+    </div>
+  )
+}
+
+export default Input
