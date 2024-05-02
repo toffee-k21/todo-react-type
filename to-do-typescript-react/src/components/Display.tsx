909b6437c37cd415bd20b71deda2e583cdf7a440
Taufiq <taufiq9.2004@gmail.com>
2024-05-02 19:16:51 +0530

    type

--- to-do-typescript-react/src/components/Display.tsx
+++ to-do-typescript-react/src/components/Display.tsx
@@ -1,28 +1,45 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Todo, todoContext } from '../utils/Context'
 
 const Display = () => {
+  const [list,setList] = useState<Todo[]>([])
   const Todos = useContext(todoContext)
-  const todoList = Todos?.todo;
-  const handleToggle = Todos?.handleToggleItems
-  const handleDeleteItem = Todos?.handleDeleteItem
+  const {todo,handleToggleItems,handleDeleteItem} = Todos!
+  // const todoList = Todos?.todo;
+  // const handleToggle = Todos?.handleToggleItems
+  // const handleDeleteItem = Todos?.handleDeleteItem
   // console.log(handleToggle())
   // const handleTodos = Todos?.handleTodo;
 
   // const [bool,setBool] = useState(false)
-  console.log(todoList)
-
+  // console.log(todoList)
+  useEffect(()=>{
+    setList(todo);
+  },[])
+const handleAll = ( )=>{
+// setList(todoList?.filter((r)=>r.completed == true))
+setList(todo)
+}
+const handleActive =()=> {
+  setList(todo?.filter((r)=>r.completed == false))
+}
+const handlecompleted = ( )=>{
+  setList(todo?.filter((r)=>r.completed == true))
+}
   return (
     <div>
+      <button onClick={handleAll}>all</button>
+      <button onClick={handleActive}>active</button>
+      <button onClick={handlecompleted}>completed</button>
       {
-        todoList?.map((r: Todo) => {
+        todo?.map((r: Todo) => {
           return <li className='list-none'>
             <input type="checkbox"
               checked={r.completed}
-              onChange={() => handleToggle(r?.id)} 
+              onChange={() => handleToggleItems(r?.id)} 
               />
-            <label>{r?.task}</label>
-            {r.completed ? <button onClick={()=>handleDeleteItem(r?.id)} className='bg-red-500 p-1 ml-10'>Delete</button>: <button></button>}
+            {r.completed? <del>{r.task}</del>:<label>{r?.task}</label>}
+            {r.completed ? <button onClick={() => handleDeleteItem(r?.id)} className='bg-red-500 p-1 ml-10'>Delete</button>: <button></button>}
           </li>
         }
 
